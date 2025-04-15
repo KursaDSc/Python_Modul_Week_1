b53fbdab0938d5312a3410efa11cc29a3b83d509
FurkanBilal <furkanbilalcubuk@gmail.com>
2025-04-15 23:13:27 +0200

    Update arithmetic_operators.py

---
+++ members/furkan/arithmetic_operators.py
@@ -0,0 +1,11 @@
+a = int(input("Bir sayi girin: "))
+b = int(input("Bir sayi girin: "))
+listtop = []
+listfark = []
+listcarp = []
+listtop = a + b
+listfark = a - b
+listcarp = a * b
+print(listtop)
+print(listfark)
+print(listcarp)
