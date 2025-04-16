238ee4f26191b99f2eaf35e4c88b2082ae359c0f
FurkanBilal <furkanbilalcubuk@gmail.com>
2025-04-17 00:06:20 +0200

    Update find_second_maximum.py

--- members/furkan/find_second_maximum.py
+++ members/furkan/find_second_maximum.py
@@ -12,3 +12,26 @@ if __name__ == '__main__':
     tablo2.remove(en)
     en2=max(tablo2)
     print(en2)
+
+
+
+
+
+
+
+
+#Buda benim VS Code üzerinde yazdığım hali:
+tablo=[]
+tablo2=[]
+a1=int(input("Yarismaci 1'in scorunu giriniz: "))
+a2=int(input("Yarismaci 2'in scorunu giriniz: "))
+a3=int(input("Yarismaci 3'in scorunu giriniz: "))
+a4=int(input("Yarismaci 4'in scorunu giriniz: "))
+a5=int(input("Yarismaci 5'in scorunu giriniz: "))
+tablo = [a1,a2,a3,a4,a5]
+tabset = set(tablo)
+tablo2 = list(tabset)
+en=max(tablo2)
+tablo2.remove(en)
+en2=max(tablo2)
+print("Runner up scoru: ",en2)
