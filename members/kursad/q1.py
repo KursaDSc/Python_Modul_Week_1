fbe361722649adc1ea4e66936250717f332b8eae
Kursad <122346253+KursaDSc@users.noreply.github.com>
2025-04-17 12:54:54 +0200

    Kursad q1.py dosyasında hata giderdi

--- members/kursad/q1.py
+++ members/kursad/q1.py
@@ -1,16 +1,2 @@
 for x in range(1,11):
     print(x, end=" ")
-
-class Araba:
-    def __init__(self, max_hiz, yakit_turu):
-        self.max_hiz = max_hiz
-        self.yakit_turu = yakit_turu
-        print(f"Yeni bir araba oluşturuldu! 🚗")
-        print(f"Maksimum Hız: {self.max_hiz} km/s")
-        print(f"Yakıt Türü: {self.yakit_turu}")
-        print("-----------------------------")
-
-# Örnek kullanım:
-araba1 = Araba(220, "Benzin")
-araba2 = Araba(180, "Dizel")
-araba3 = Araba(250, "Elektrik")
