dc90bedbf70521e48260ab6a95b6f9bf42a66638
MLAcikgoz <acikgozmehmetlutfu34@gmail.com>
2025-04-17 14:38:22 +0200

    Update q11.py

---
+++ members/mehmet/q11.py
@@ -0,0 +1,7 @@
+#buyuk sayiyi bul
+a = float(input("1. sayıyı girin: "))
+b = float(input("2. sayıyı girin: "))
+c = float(input("3. sayıyı girin: "))
+#sayilari belirledik
+en_buyuk = max(a, b, c)
+print("En büyük sayı:", en_buyuk)
